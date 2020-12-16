dcbc531c6815e5e3bb2edf56b456c0113c02cc0f
Harm Kolk <hkolk@ebay.com>
2020-12-16 07:41:26 +0100

    Day16 Part1

--- src/main/kotlin/nl/hkolk/aoc2020/Day16.kt
+++ src/main/kotlin/nl/hkolk/aoc2020/Day16.kt
@@ -1,4 +1,88 @@
 package nl.hkolk.aoc2020
 
-class Day16 {
+import java.lang.IllegalArgumentException
+
+class Day16(input: List<String>) {
+    fun List<String>.splitBy(func: (String) -> Boolean): List<List<String>> {
+        val ret = mutableListOf<List<String>>()
+        var collect = mutableListOf<String>()
+        for(line in this) {
+            if(func(line)) {
+                ret.add(collect)
+                collect = mutableListOf()
+            } else {
+                collect.add(line)
+            }
+        }
+        if(collect.isNotEmpty()) ret.add(collect)
+        return ret
+    }
+
+    data class Validation(val name: String, val ranges: List<IntRange>) {
+        fun match(value: Int): Boolean {
+            for(range in ranges) {
+                if(value in range) {
+                    return true
+                }
+            }
+            return false
+        }
+
+        fun validate(ticket: List<Int>): Int {
+            var errors = 0
+            for(value in ticket) {
+                if(!match(value)) {
+                    errors += value
+                }
+            }
+            return errors
+        }
+
+        companion object {
+            fun fromLine(line: String): Validation {
+                val subParts = line.split(":", "-", " or ")
+                val name = subParts[0]
+                val ranges = subParts.drop(1).map {
+                    it.trim().toIntOrNull() ?: throw IllegalArgumentException("Bad value: '$it' in '$line'")
+                }.chunked(2).map{IntRange(it.first(), it.last())}
+                return Validation(name, ranges)
+            }
+        }
+    }
+
+    val validations: List<Validation>
+    val myTicket: List<Int>
+    val tickets: List<List<Int>>
+    val validTickets: List<List<Int>>
+    init {
+        val parts = input.splitBy( {it.isNullOrEmpty()} )
+        validations = parts[0].map { Validation.fromLine(it) }
+
+        myTicket = parts[1].last().split(",").map { it.toIntOrNull() ?: throw IllegalArgumentException("$it is not an integer") }
+
+        tickets = parts[2].drop(1).map { line ->
+            line.split(",").map { it.toIntOrNull() ?: throw IllegalArgumentException("$it is not an integer") }
+        }
+        validTickets = tickets.filter { ticket ->
+            validations.map { validation -> validation.validate(ticket) }.sum() == 0
+        }
+    }
+
+    fun solvePart1(): Int {
+        println(tickets)
+        println(validations)
+        var ret = 0
+        for(ticket in tickets) {
+            for(value in ticket) {
+                if(validations.map { it.match(value) }.filter { it }.isEmpty()) {
+                    ret += value
+                    println("Invalid: $value")
+                }
+            }
+        }
+        return ret
+    }
+    fun solvePart2(): Int {
+        TODO()
+    }
 }
--- src/test/kotlin/nl/hkolk/aoc2020/Day16Test.kt
+++ src/test/kotlin/nl/hkolk/aoc2020/Day16Test.kt
@@ -1,4 +1,43 @@
 package nl.hkolk.aoc2020
 
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+
+@DisplayName("Day 16")
 class Day16Test {
+
+    val testInput = Resources.resourceAsList("day16_test.txt")
+    val realInput = Resources.resourceAsList("day16.txt")
+
+    @Nested
+    @DisplayName("Part 1")
+    inner class Part1 {
+
+        @Test
+        fun `Matches Example`() {
+            val answer = Day16(testInput).solvePart1()
+            Assertions.assertThat(answer).isEqualTo(71)
+        }
+        @Test
+        fun `Actual Answer`() {
+            val answer = Day16(realInput).solvePart1()
+            Assertions.assertThat(answer).isEqualTo(24021)
+        }
+    }
+    @Nested
+    @DisplayName("Part 2")
+    inner class Part2 {
+        @Test
+        fun `Matches Example`() {
+            val answer = Day16(testInput).solvePart2()
+            Assertions.assertThat(answer).isEqualTo(286)
+        }
+        @Test
+        fun `Actual Answer`() {
+            val answer = Day16(realInput).solvePart2()
+            Assertions.assertThat(answer).isEqualTo(38693)
+        }
+    }
 }
